50f7e85f2d22951d868a1f314b8a1f22ddbbf43a
Mayur Gaud <mayur_gaud99@yahoo.com>
2017-05-09 20:25:25 +0200

    Second commit

--- api/controllers/stripController.js
+++ api/controllers/stripController.js
@@ -11,38 +11,10 @@ exports.list_all_strips = function(req, res) {
     });
 };
 
-exports.create_a_task = function(req, res) {
-    var new_task = new Task(req.body);
-    new_task.save(function(err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
-};
-
 exports.read_a_task = function(req, res) {
     Task.findById(req.params.taskId, function(err, task) {
         if (err)
             res.send(err);
         res.json(task);
-    });
-};
-
-exports.update_a_task = function(req, res) {
-    Task.findOneAndUpdate(req.params.taskId, req.body, {new: true}, function(err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
-};
-// Task.remove({}).exec(function(){});
-exports.delete_a_task = function(req, res) {
-
-    Task.remove({
-        _id: req.params.taskId
-    }, function(err, task) {
-        if (err)
-            res.send(err);
-        res.json({ message: 'Task successfully deleted' });
     });
 };
--- api/routes/stripRoutes.js
+++ api/routes/stripRoutes.js
@@ -6,9 +6,4 @@ module.exports = function(app) {
     // todoList Routes
     app.route('/strips')
         .get(stripList.list_all_strips);
-
-    app.route('/tasks/:taskId')
-        .get(stripList.read_a_task)
-        .put(stripList.update_a_task)
-        .delete(stripList.delete_a_task);
 };

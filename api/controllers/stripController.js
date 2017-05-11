5b7a90f75a3578231a7eb8f9883b8f2875653264
Mayur Gaud <mayur_gaud99@yahoo.com>
2017-05-11 15:29:53 +0200

    Corrected the api offset

--- api/controllers/stripController.js
+++ api/controllers/stripController.js
@@ -7,28 +7,39 @@ exports.list_all_strips = function (req, res) {
     var year = req.query.year;
     var month = req.query.month;
     var limit = 5;
+    var offset = req.query.offset ? req.query.offset : 0;
 
     if (year && month) {
-        Task.find({imageYear: year, imageMonth: month}, {}, {limit: limit}, function (err, task) {
+        Task.find({imageYear: year, imageMonth: month}, {}, {
+            skip: parseInt(offset),
+            limit: limit
+        }, function (err, task) {
             if (err)
                 res.send(err);
             res.json(task);
         });
     } else if (year) {
         console.log(year);
-        Task.find({imageYear: year}, {}, {sort: {"imageMonth": -1}, limit: limit}, function (err, task) {
+        Task.find({imageYear: year}, {}, {
+            sort: {"imageMonth": -1},
+            skip: parseInt(offset),
+            limit: limit
+        }, function (err, task) {
             if (err)
                 res.send(err);
             res.json(task);
         });
     } else if (month) {
-        Task.find({imageMonth: month}, {}, {sort: {"imageYear": -1}, limit: limit}, function (err, task) {
+        Task.find({imageMonth: month}, {}, {
+            sort: {"imageYear": -1},
+            skip: parseInt(offset),
+            limit: limit
+        }, function (err, task) {
             if (err)
                 res.send(err);
             res.json(task);
         });
     } else {
-        var offset = req.query.offset ? req.query.offset : 0;
         Task.find({}, {}, {
                 sort: {"imageYear": -1, "imageMonth": -1},
                 skip: parseInt(offset), limit: limit

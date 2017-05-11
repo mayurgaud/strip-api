0b99557306877d0f10fc87170a91c035cb7b9483
Mayur Gaud <mayur_gaud99@yahoo.com>
2017-05-11 13:27:38 +0200

    Made some changes in the controller

--- api/controllers/stripController.js
+++ api/controllers/stripController.js
@@ -3,16 +3,47 @@
 var mongoose = require('mongoose'),
     Task = mongoose.model('strips');
 
-exports.list_all_strips = function(req, res) {
-    Task.find({}, function(err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
+exports.list_all_strips = function (req, res) {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    var year = req.query.year;
+    var month = req.query.month;
+    var limit = 5;
+
+    if (year && month) {
+        Task.find({imageYear: year, imageMonth: month}, {}, {limit: limit}, function (err, task) {
+            if (err)
+                res.send(err);
+            res.json(task);
+        });
+    } else if (year) {
+        Task.find({imageYear: year}, function (err, task) {
+            if (err)
+                res.send(err);
+            res.json(task);
+        });
+    } else if (month) {
+        Task.find({imageMonth: month}, {}, {limit: limit}, function (err, task) {
+            if (err)
+                res.send(err);
+            res.json(task);
+        });
+    } else {
+        var offset = req.query.offset ? req.query.offset : 0;
+        Task.find({}, {}, {skip: parseInt(offset), limit: limit}, function (err, task) {
+            if (err){
+                return res.send(err);
+            }
+            res.json(task);
+        });
+    }
 };
 
-exports.read_a_task = function(req, res) {
-    Task.findById(req.params.taskId, function(err, task) {
+exports.read_a_task = function (req, res) {
+    Task.findById(req.params.taskId, function (err, task) {
         if (err)
             res.send(err);
         res.json(task);

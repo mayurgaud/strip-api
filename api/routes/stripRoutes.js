3d8e65837e63d6dd4095074fde7775119457e053
Mayur Gaud <mayur_gaud99@yahoo.com>
2017-06-01 18:18:29 +0200

    Removed the user addition code

--- api/routes/stripRoutes.js
+++ api/routes/stripRoutes.js
@@ -3,7 +3,7 @@
 module.exports = function (app) {
     var stripList = require('../controllers/stripController');
     var user = require('../controllers/userController');
-    // add user route
+
     app.route('/authenticate').post(user.authenticate);
     // todoList Routes
     app.route('/strips')
